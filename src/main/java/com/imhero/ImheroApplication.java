6d098a2e3480fdf1c92bdc39c6f392dfd36613ef
mtae616 <mtae616@naver.com>
2023-05-29 17:31:06 +0900

    initial project

---
+++ src/main/java/com/imhero/ImheroApplication.java
@@ -0,0 +1,13 @@
+package com.imhero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImheroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imhero/ImheroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.imhero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@SpringBootTest
+class ImheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
