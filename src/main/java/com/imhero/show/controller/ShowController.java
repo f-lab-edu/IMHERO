4ec8d8bcb99b3ef6e8528d904de8e7db70281535
Tae-Su Kang <mtae616@naver.com>
2023-06-24 17:39:43 +0900

    [#19] style: log 삭제
    log 를 삭제하였습니다.

--- src/main/java/com/imhero/show/controller/ShowController.java
+++ src/main/java/com/imhero/show/controller/ShowController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -31,7 +30,6 @@ public class ShowController {
 
     @PostMapping("")
     public Response<Long> save(@RequestBody ShowRequest showRequest) {
-        log.info("the request is {}", showRequest);
         return Response.success(showService.save(showRequest));
     }
 
