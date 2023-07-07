08a11d381fa389b2d26e9c2583410d8dff1f38cb
Tae-Su Kang <mtae616@naver.com>
2023-07-07 22:38:18 +0900

    [#30] fix: 쿼리 및 dto 수정
    entity 를 담는 대신 개별 컬럼으로 수정하였습니다.

--- src/main/java/com/imhero/reservation/repository/ReservationRepository.java
+++ src/main/java/com/imhero/reservation/repository/ReservationRepository.java
@@ -1,8 +1,8 @@
 package com.imhero.reservation.repository;
 
 import com.imhero.reservation.domain.Reservation;
-import com.imhero.reservation.dto.ReservationDao;
-import com.imhero.reservation.dto.ReservationSellerDao;
+import com.imhero.reservation.dto.ReservationDto;
+import com.imhero.reservation.dto.ReservationSellerDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -24,21 +24,38 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     int updateDelYnByIds(@Param("ids") Set<Long> ids);
 
     @Query("SELECT" +
-            " new com.imhero.reservation.dto.ReservationDao(r.user, r.seat.showDetail.show, r.seat.showDetail, r.seat, r)" +
+            " new com.imhero.reservation.dto.ReservationDto(" +
+            " r.user.id, r.user.email, r.user.username," +
+            " r.seat.showDetail.show.id, r.seat.showDetail.show.artist, r.seat.showDetail.show.place," +
+            " r.seat.showDetail.show.title, r.seat.showDetail.show.createdBy, " +
+            " r.seat.showDetail.show.showFromDate, r.seat.showDetail.show.showToDate, r.seat.showDetail.show.delYn," +
+            " r.seat.showDetail.id, r.seat.showDetail.reservationFromDt, r.seat.showDetail.reservationToDt," +
+            " r.seat.showDetail.sequence, r.seat.showDetail.delYn," +
+            " r.seat.id, r.seat.gradeDetails.grade, r.seat.gradeDetails.price, r.seat.currentQuantity, r.seat.totalQuantity," +
+            " r.id, r.delYn, r.seat.id" +
+            ")" +
             " FROM Reservation r" +
             " JOIN r.seat" +
             " JOIN r.user" +
             " JOIN r.seat.showDetail" +
             " JOIN r.seat.showDetail.show" +
             " where r.user.email = :email")
-    List<ReservationDao> findAllReservationByEmail(@Param("email") String email);
+    List<ReservationDto> findAllReservationByEmail(@Param("email") String email);
 
     @Query("SELECT" +
-            " new com.imhero.reservation.dto.ReservationSellerDao(s.showDetail.show, s.showDetail, s.showDetail.show.user, s)" +
+            " new com.imhero.reservation.dto.ReservationSellerDto(" +
+            " s.showDetail.show.user.id, s.showDetail.show.user.email, s.showDetail.show.user.username," +
+            " s.showDetail.show.id, s.showDetail.show.artist, s.showDetail.show.place," +
+            " s.showDetail.show.title, s.showDetail.show.createdBy, " +
+            " s.showDetail.show.showFromDate, s.showDetail.show.showToDate, s.showDetail.show.delYn," +
+            " s.showDetail.id, s.showDetail.reservationFromDt, s.showDetail.reservationToDt," +
+            " s.showDetail.sequence, s.showDetail.delYn," +
+            " s.id, s.gradeDetails.grade, s.gradeDetails.price, s.currentQuantity, s.totalQuantity" +
+            ")" +
             " FROM Seat s" +
             " JOIN s.showDetail" +
             " JOIN s.showDetail.show" +
             " JOIN s.showDetail.show.user" +
             " where s.showDetail.show.user.email = :email")
-    List<ReservationSellerDao> findAllSeatByEmail(@Param("email") String email);
+    List<ReservationSellerDto> findAllSeatByEmail(@Param("email") String email);
 }
